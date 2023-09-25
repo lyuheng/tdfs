477e6ce3738f9022edeb7ff8128643d778f93ad8
Lyuheng Yuan <lyuheng@Lyuhengs-MBP.lan>
2023-09-25 15:02:52 -0500

    fix

--- src/gpu_match.cu
+++ src/gpu_match.cu
@@ -276,30 +276,29 @@ namespace STMatch
 					int cond = arg->pat->condition_order[actual_lvl * PAT_SIZE * 2 + 2 * k];
 					int cond_lvl = arg->pat->condition_order[actual_lvl * PAT_SIZE * 2 + 2 * k + 1];
 					int cond_vertex_M = path(stk, arg->pat, cond_lvl - 1);
-					// if (cond == CondOperator::LESS_THAN) {
-					// 	if (cond_vertex_M >= target) {
-					// 		pred = false;
-					// 		break;
-					// 	}
-					// }
-					// else if (cond == CondOperator::LARGER_THAN) {
-					// 	if (cond_vertex_M <= target) {
-					// 		pred = false;
-					// 		break;
-					// 	}
-					// }
-					// else if (cond == CondOperator::NON_EQUAL) {
+					if (cond == CondOperator::LESS_THAN) {
+						if (cond_vertex_M <= target) {
+							pred = false;
+							break;
+						}
+					}
+					else if (cond == CondOperator::LARGER_THAN) {
+						if (cond_vertex_M >= target) {
+							pred = false;
+							break;
+						}
+					}
+					else if (cond == CondOperator::NON_EQUAL) {
 						if (cond_vertex_M == target) {
 							pred = false;
 							break;
 						}
-					// }
+					}
 				}
 				if (pred) pred = bsearch_exist(arg->set1, arg->set1_size, target);
 			}
 			int loc = scanIndex(pred) + res_length;
-			if ((arg->level < arg->pat->nnodes - 2 && pred) || ((arg->level == arg->pat->nnodes - 2) && !last_round && pred)
-				|| (arg->level == arg->pat->nnodes - 2) && last_round)
+			if ((arg->level < arg->pat->nnodes - 2 && pred) || ((arg->level == arg->pat->nnodes - 2) && !last_round && pred))
 				arg->res[loc] = target;
 			if (threadIdx.x % WARP_SIZE == 31) // last lane's loc+pred is number of items found in this scan
 				res_length = loc + pred;
