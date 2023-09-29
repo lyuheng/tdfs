e57579205a84a249b9bc94434a3d1d5fa57feb08
Lyuheng Yuan <lyuheng@Lyuhengs-MBP.lan>
2023-09-29 18:29:50 -0500

    fix

--- src/gpu_match.cu
+++ src/gpu_match.cu
@@ -264,6 +264,7 @@ namespace STMatch
 		bool pred;
 		int target;
 		int cur_label = arg->pat->vertex_labels[actual_lvl];
+		assert(cur_label == 1);
 		for (int i = 0; i < arg->set2_size; i += WARP_SIZE)
 		{
 			pred = false;
