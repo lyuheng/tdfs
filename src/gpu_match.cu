00c65e421d957781248ea83b383c7533cadfb161
Lyuheng Yuan <lyuheng@Lyuhengs-MBP.lan>
2023-10-04 21:55:12 -0500

    fix

--- src/gpu_match.cu
+++ src/gpu_match.cu
@@ -406,8 +406,7 @@ namespace STMatch
 				}
 			}
 			int loc = scanIndex(pred) + res_length;
-			// if (arg->level < arg->pat->nnodes - 2 && pred
-			if (pred)
+			if (arg->level < arg->pat->nnodes - 2 && pred)
 				stk->slot_storage[arg->level][loc] = target;
 			if (threadIdx.x % WARP_SIZE == 31) // last lane's loc+pred is number of items found in this scan
 				res_length = loc + pred;
