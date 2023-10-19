04218e385c8dd904f97effe7ef86282a209dbff1
Lyuheng Yuan <lyuheng@Lyuhengs-MBP.lan>
2023-10-18 23:42:36 -0500

    fix

--- src/gpu_match.cuh
+++ src/gpu_match.cuh
@@ -7,10 +7,13 @@
 #include "queue.h"
 
 namespace STMatch {
-  template <typename MemoryManagerType>
-  __global__ void _parallel_match(MemoryManagerType *mm, Graph* dev_graph, Pattern* dev_pattern, 
+
+template <typename MemoryManagerType>
+__global__ void _parallel_match(MemoryManagerType *mm, Graph* dev_graph, Pattern* dev_pattern, 
                             CallStack* dev_callstack, JobQueue* job_queue,  size_t* res,
                             int* idle_warps, int* idle_warps_count, int* global_lock,
                             Queue *queue);
 
+template <typename MemoryManagerType>
+__global__ void allocate_memory(MemoryManagerType *mm, CallStack *dev_callstack);
 }
