7353d019071b94dfabed5c0811744fb97804f1fe
Lyuheng Yuan <lvhengy@gmail.com>
2023-10-25 15:51:50 -0500

    fix

--- src/pattern.h
+++ src/pattern.h
@@ -379,7 +379,7 @@ inline std::string GetCondOperatorString(const CondOperator& op) {
         }
         for(int j = i-1; j >= 1; --j)
         {
-          if (nbr_bits[i] & nbr_bits[j] == nbr_bits[j] 
+          if ((nbr_bits[i] & nbr_bits[j]) == nbr_bits[j] 
           && pat.num_BN[i] >= 2 && pat.num_BN[j] >= 2)
           {
             std::cout << nbr_bits[i] << " " << nbr_bits[j] << " " << int(nbr_bits[i] & nbr_bits[j]) << std::endl;
